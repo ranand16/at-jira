1c0c98ca177012855b80d0faf16f3bcbca85d4fe
Rishabh Anand <ranand16@gmail.com>
2023-10-27 20:26:48 +0530

    refactor(codebase): codebase
    codebase

--- src/app/(auth)/dashboard/layout.tsx
+++ src/app/(auth)/dashboard/layout.tsx
@@ -55,13 +55,7 @@ export default function DashboardLayout({
 
         <footer className="border-t border-gray-300 py-8 text-center text-sm">
           © Copyright {new Date().getFullYear()} {AppConfig.title}. Made with{' '}
-          <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>.
-          {/*
-           * PLEASE READ THIS SECTION
-           * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
-           * The link doesn't need to appear on every pages, one link on one page is enough.
-           * For example, in the `About` page. Thank you for your support, it'll mean a lot to me.
-           */}
+          <a href="https://rishabh-anand.dev">Rishabh Anand</a>.
         </footer>
       </div>
     </div>
